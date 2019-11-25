d2d3367ec0be78a95bddb227b12aadeebed97c06
worlak <worlak2@mail.ru>
2019-11-25 14:51:32 +0300

    Парсер Airforce завершен.

--- database.py
+++ database.py
@@ -6,18 +6,33 @@ db = SqliteDatabase('my_app.db')
 class Base(Model):
     class Meta:
         database = db
+        database.timeout = 5
 
 
 class SiteData(Base):
     unique_id = PrimaryKeyField()
     url = CharField()
+    title = CharField()
     data_site = TextField()
-    name = CharField()
     date_upload = DateTimeField()
 
 
 class SiteFiles(Base):
     unique_id = PrimaryKeyField()
-    images_path = CharField()
-    video_path = CharField()
+    images_path = CharField(null=True)
     key = ForeignKeyField(SiteData, to_field="unique_id", on_delete='CASCADE')
+
+
+def insert(url: str, title: str, data_site: str, date_upload, images_path):
+    db.create_tables([SiteData, SiteFiles])
+    site_data = SiteData.create(url=url, title=title, data_site=data_site, date_upload=date_upload)
+    SiteFiles(images_path=images_path, key=site_data).save()
+
+
+def check_data(url):
+    db.create_tables([SiteData, SiteFiles])
+    query = SiteData.select().where(SiteData.url == str(url))
+    if not query.exists():
+        return False
+    else:
+        return True
--- parser.py
+++ parser.py
@@ -1,39 +1,32 @@
 from abc import ABC, abstractmethod
+
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
+import multiprocessing.pool
+from database import insert, db,check_data
+import os
+import sys
+from pathlib import Path
+import shutil
 import requests
 
 
+class NoDaemonProcess(multiprocessing.Process):
+    def _get_daemon(self):
+        return False
+
+    def _set_daemon(self, value):
+        pass
+
+    daemon = property(_get_daemon, _set_daemon)
+
+
+class MyPool(multiprocessing.pool.Pool):
+    Process = NoDaemonProcess
+
+
 class BaseParse(ABC):
-
-    @abstractmethod
-    def get_data_site(self):
-        pass
-
-    @abstractmethod
-    def get_articles(self):
-        pass
-
-    @abstractmethod
-    def parse_data(self):
-        pass
-
-    @abstractmethod
-    def parse_article(self, url):
-        pass
-
-    @abstractmethod
-    def download_image(self, url):
-        pass
-
-    def insert_to_database(self):
-        pass
-
-
-class ParseAirforce(BaseParse):
-    def __init__(self, url):
-        self.url = url
-        self._process_count = 20
+    _process_count = 20
 
     @property
     def process_count(self):
@@ -48,43 +41,130 @@ class ParseAirforce(BaseParse):
         else:
             raise ValueError(f'Failed parse value {value}. Failed format.')
 
-    def get_data_site(self, addition_url=None):
-        if addition_url:
-            return requests.get(addition_url).text
-        return requests.get(self.url).text
-
-    def get_articles(self):
-        article = BeautifulSoup(self.get_data_site(), 'lxml').find('ul',
-                                                                   {'class': 'category_list_new pal list'}).findAll(
-            href=True)
-        return article if len(article) > 0 else None
-
-    def download_image(self, url):
+    @abstractmethod
+    def get_data_site(self, url):
         pass
 
-    def parse_article(self, *args):
-        data = BeautifulSoup(self.get_data_site(addition_url=args[0]['url']), 'lxml')
+    @abstractmethod
+    def get_articles(self, url):
+        pass
+
+    @abstractmethod
+    def parse_article(self, url):
+        pass
+
+    @abstractmethod
+    def parse_all_data(self):
+        pass
+
+    @abstractmethod
+    def check_new(self):
+        pass
+
+    def insert_to_database(self, data):
+        with db.atomic() as transaction:
+            try:
+                for i in data:insert(**i)
+            except Exception as e:
+                print(e)
+                transaction.rollback()
+
+    def parse_data(self, url):
+        parsed_article = self.get_articles(url)
+        if parsed_article:
+            with Pool(self._process_count) as p:
+                release = p.map(self.parse_article, [{'url': i['href'], 'title': i.text} for i in parsed_article])
+            self.insert_to_database(release)
+
+
+class BaseParseArticle(ABC):
+
+    @abstractmethod
+    def parse_article(self, args, data):
+        pass
+
+
+class BaseImageSave:
+    def __init__(self, path):
+        self.base_path = path
+
+    def rename_image(self, article):
+        return f'{hash(article)}.jpg'
+
+    def download_image(self, url, article):
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            path = Path("media/images/") / self.base_path / article
+            os.makedirs(path, exist_ok=True)
+            path = path / self.rename_image(article)
+            with open(path, 'wb') as f:
+                response.raw.decode_content = True
+                shutil.copyfileobj(response.raw, f)
+            return path
+        else:
+            return None
+
+    def save_image(self, url, article):
+        image = self.download_image(url, article)
+        return image
+
+
+class ParseAirforceArticle(BaseParseArticle):
+
+    def parse_article(self, args, data):
         parsed_data = data.find('div', {
             'class': 'post-content cf'}).findAll('p')
 
         parsed_list = [i.text for i in parsed_data[:-1]]
         parsed_list[-1] = parsed_list[-1][:parsed_list[-1].find('\n')]
+        date_publication = data.find('div', {'class': 'article-date'})
+
+        image = data.find('div', {'class': 'share-container'}).findAll('img')
+        image = image[0] if len(image) > 1 else None
+        work_with_image = BaseImageSave('Airforce').save_image(image['src'], args['title']) if image else None
+
+        return {'url': args['url'], 'title': args['title'], 'images_path': work_with_image,
+                'data_site': ''.join(parsed_list), 'date_upload': date_publication.text}
 
 
-        image = data.find('div',{'class':'share-container'}).findAll('img')[0]
-        print(image['src'])
+class ParseAirforce(BaseParse):
 
-        return args[0]['url'], args[0]['title']
+    def get_data_site(self, url):
+        return requests.get(url).text
 
+    def get_articles(self, url):
+        article = BeautifulSoup(self.get_data_site(url), 'lxml').find('ul',
+                                                                      {'class': 'category_list_new pal list'}).findAll(
+            href=True)
+        return article if len(article) > 0 else None
 
-    def parse_data(self):
-        parsed_article = self.get_articles()
-        if parsed_article:
-            with Pool(self._process_count) as p:
-                release = p.map(self.parse_article, [{'url': i['href'], 'title': i.text} for i in parsed_article])
-                print(release)
+    def parse_article(self, args):
+        data = BeautifulSoup(self.get_data_site(url=args['url']), 'lxml')
+        parsed_article = ParseAirforceArticle().parse_article(args, data)
+        return parsed_article
+
+    def check_new(self):
+        sys.setrecursionlimit(25000)
+        to_parse=[]
+        with Pool(20) as p:
+            result = p.map(self.get_articles, ['https://www.airforce-technology.com/news/page/{0}/'.format(i) for i in range(1,50)])
+            for i in result:
+                for j in i:
+                    if not check_data(j['href']):
+                        to_parse.append(j)
+            release = p.map(self.parse_article, [{'url': i['href'], 'title': i.text} for i in to_parse])
+        self.insert_to_database(release)
+
+    def parse_all_data(self):
+        pool = MyPool(6)
+        pool.map(self.parse_data,
+                 [f'https://www.airforce-technology.com/news/page/{i}/' for i in range(1, 50)])
+        pool.close()
+        pool.join()
 
 
 if __name__ == '__main__':
-    result = ParseAirforce('https://www.airforce-technology.com/news/')
-    result.parse_data()
+    result = ParseAirforce()
+    # result.parse_all_data()
+    result.check_new()
+
